7ab4aeb14bad7baea34da9be813e7c73f9e48477
Gabriel San Martin <gssm.snf19@uea.edu.br>
2020-01-11 14:14:11 -0400

    Initial commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'gasmartin',
+    password: 'root',
+    database: 'chickentracker',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
